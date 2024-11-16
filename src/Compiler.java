2c9a1119b1ac1eadf4e90f0c7064a72f94573158
Toryn <toryn@Toryns-MacBook-Pro.local>
2024-11-16 11:57:08 +0800

    fc

--- src/Compiler.java
+++ src/Compiler.java
@@ -13,44 +13,44 @@ public class Compiler {
     public static void main(String[] args) throws IOException {
         // 1. 源程序文件，解析结果存储在字符串中
         String inputFile = "testfile.txt";
-
-        // 2. Lexer解析源文件，结果写入字符串中
+        String grammarAnalysisFile = "parser.txt";
+        String symbolTableFile = "symbol.txt";
+        String errorHandlerFile = "error.txt";
         BufferedReader br = new BufferedReader(new FileReader(inputFile));
+        FileWriter fwGrammar = new FileWriter(grammarAnalysisFile);
+        FileWriter fwSymbol = new FileWriter(symbolTableFile);
+        FileWriter fwErr = new FileWriter(errorHandlerFile);
+
+        // 2. 词法分析
         Lexer lexer = new Lexer(br);
 
-        // 3. 打印结果
-        String outputFile = "parser.txt";
-        FileWriter fwOut = new FileWriter(outputFile);
-        Parser parser = new Parser(lexer,fwOut);
+        // 3. 语法分析
+        Parser parser = new Parser(lexer,fwGrammar);
         parser.parseCompUnit();
-        fwOut.close();
+        fwGrammar.close();
 
-        // 4. errors
-        String errFile = "error.txt";
-        FileWriter fwErr = new FileWriter(errFile);
+        // 4. 语义分析
+        // 4.1 全局
+        int scope = 1;
+        SymbolTable symbolTable = parser.getSymbolTable();
+        for(SymbolItem<DefNode> item:symbolTable.getVariables()){
+            fwSymbol.write(scope + " " + item.getName() + " " + item.getNode().getDefNodeType().toString() + "\n");
+        }
+        for(SymbolItem<FuncDefNode> item:symbolTable.getFunctions()){
+            fwSymbol.write(scope + " " + item.getName() + " " + item.getNode().getFuncDefType().toString() + "\n");
+        }
+        // 4.2 逐个
+        for(SymbolTable child:symbolTable.getChildren()){
+            scope++;
+            scope = child.print(fwSymbol,scope);
+        }
+        fwSymbol.close();
+
+        // 5. 错误处理
         ErrorHandler.getInstance().sortErrorsByLineNumber();
         for (ErrorRecord errorRecord : ErrorHandler.getInstance().getErrors()) {
             fwErr.write(errorRecord + "\n");
         }
         fwErr.close();
-
-        // 5. symbol
-        String symFile = "symbol.txt";
-        FileWriter fwSym = new FileWriter(symFile);
-        // 5.1 全局
-        int scope = 1;
-        SymbolTable symbolTable = parser.getSymbolTable();
-        for(SymbolItem<DefNode> item:symbolTable.getVariables()){
-            fwSym.write(scope + " " + item.getName() + " " + item.getNode().getDefNodeType().toString() + "\n");
-        }
-        for(SymbolItem<FuncDefNode> item:symbolTable.getFunctions()){
-            fwSym.write(scope + " " + item.getName() + " " + item.getNode().getFuncDefType().toString() + "\n");
-        }
-        // 5.2 逐个
-        for(SymbolTable child:symbolTable.getChildren()){
-            scope++;
-            scope = child.print(fwSym,scope);
-        }
-        fwSym.close();
     }
 }
