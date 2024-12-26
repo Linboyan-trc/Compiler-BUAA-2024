8ff80ccbede709ab2a2f1aa0d049ede795d4f48f
Toryn <1584340372@qq.com>
2024-12-26 11:44:41 +0800

    cm

--- testfile.c
+++ testfile.c
@@ -1,6 +1,38 @@
 int main()
 {
-	int i = getint(), j = getint();
-	printf("%d", i bitand j);
+	int a0 = 1;
+    int a1 = 1;
+    int a2 = 2;
+    int a3 = 2;
+    int a4 = 4;
+    int a5 = 4;
+    int a6 = 8;
+    int a7 = 8;
+    int a8 = 16;
+    int a9 = 16;
+    int a10 = 32;
+    int a11 = 32;
+    int a12 = 64;
+    int a13 = 64;
+    int a14 = 128;
+    int a15 = 128;
+    int a16 = 256;
+    int a17 = 256;
+    int a18 = 512;
+    int a19 = 512;
+    int a20 = 1024;
+    int a21 = 1024;
+    int a22 = 2048;
+    int a23 = 2048;
+    int a24 = 4096;
+    int a25 = 4096;
+    int a26 = 8192;
+    int a27 = 8192;
+    int a28 = 16384;
+    int a29 = 16384;
+    int a30 = 32768;
+
+    int b = a0+a10+a20+a30;
+
 	return 0;
 }
